81df39a1776725032d80824b94899361d8edc736
alejopua <alejopua@gmail.com>
2025-03-06 21:46:15 -0500

    Add middleware and routes to Express server setup

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import routes from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api", routes);
 
 try {
   const PORT = process.env.PORT || 3000;
