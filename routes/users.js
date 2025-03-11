0e706be489c934d25d0fc7c1e678ff23db5c37fd
alejopua <alejopua@gmail.com>
2025-03-10 19:29:41 -0500

    Simplify user routes to only include registration and login endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,9 @@
 import express from "express";
-import {
-  getAllUsers,
-  getUserById,
-  registerUser,
-  loginUser,
-  updateUser,
-  deleteUser,
-} from "../controllers/userController.js";
+import { registerUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/", getAllUsers); // Get all users
-router.get("/:id", getUserById); // Get user profile
-router.post("/login", loginUser); // Login user
-router.put("/:id", updateUser); // Update user
-router.delete("/:id", deleteUser); // Delete user
-router.post("/register", registerUser); // Register new user
-
+// User routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 export default router;
